0f0c6c63451d0fcd5c7e499114b4006c1f7467d3
Michael Stowe <mkstowe@umich.edu>
2021-07-28 22:54:07 -0400

    Cloud blog added

--- projects/portfolio/src/app/app-routing.module.ts
+++ projects/portfolio/src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { CloudComponent } from './cloud/cloud/cloud.component';
+import { GoldComponent } from './cloud/cloud/gold/gold.component';
+import { PreAlphaIComponent } from './cloud/cloud/pre-alpha-i/pre-alpha-i.component';
+import { PreAlphaIIComponent } from './cloud/cloud/pre-alpha-ii/pre-alpha-ii.component';
+import { PreAlphaIIIPreBetaIComponent } from './cloud/cloud/pre-alpha-iii-pre-beta-i/pre-alpha-iii-pre-beta-i.component';
+import { PreBetaIPreBetaIIComponent } from './cloud/cloud/pre-beta-i-pre-beta-ii/pre-beta-i-pre-beta-ii.component';
 import { ExperiencePageComponent } from './experience-page/experience-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { SkillsPageComponent } from './skills-page/skills-page.component';
@@ -10,6 +16,12 @@ const routes: Routes = [
   { path: 'experience', component: ExperiencePageComponent, data: { tab: 2 } },
   { path: 'skills', component: SkillsPageComponent, data: { tab: 3 } },
   { path: 'projects', component: ProjectsPageComponent, data: { tab: 4 } },
+  { path: 'projects/cloud', component: CloudComponent },
+  { path: 'projects/cloud/pre-alpha-i', component: PreAlphaIComponent },
+  { path: 'projects/cloud/pre-alpha-ii', component: PreAlphaIIComponent },
+  { path: 'projects/cloud/pre-alpha-iii-pre-beta-i', component: PreAlphaIIIPreBetaIComponent },
+  { path: 'projects/cloud/pre-beta-i-pre-beta-ii', component: PreBetaIPreBetaIIComponent },
+  { path: 'projects/cloud/gold', component: GoldComponent },
 ];
 
 @NgModule({
